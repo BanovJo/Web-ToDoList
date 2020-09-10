14175fe98957fb86a7ad5b3ec009bb17b3e1a002
banov.josipa <banov.josipa@gmail.com>
2020-09-10 21:45:35 +0200

    bla1

---
+++ WebToDoList/wwwroot/js/taskType.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/TeamLeader/TaskType/GetAll"
+        },
+        "columns": [
+            { "data": "type", "width": "60%" },
+            {
+                "data": "taskTypeId",
+                "render": function (data) {
+                    return `
+<div class="text-center">
+    <a href="/TemLeader/TaskType/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+        <i class="fas fa-edit"></i> 
+    </a>
+    <a class="btn btn-danger text-white" style="cursor:pointer">
+        <i class="fas fa-trash"></i>
+    </a>
+</div>
+                         `;
+                      
+                }, "width": "40%"
+            }
+        ]
+    });
+}
